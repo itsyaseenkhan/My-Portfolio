8f9e6e59e89134bedf88c33971be39c4f50988ad
Yaseen Khan <gul494419@gmail.com>
2025-08-15 11:00:17 +0500

    delete

--- Backends/Models/SkillFoam.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const skillSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  percentage: { type: Number, required: true },
-  type: { type: String, enum: ['technical', 'professional'], required: true }
-});
-
-module.exports = mongoose.model('Skill', skillSchema);
